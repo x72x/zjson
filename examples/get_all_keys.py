bce437fc11445446e74cfee6371589d3a2ac7f00
Zaid _ <117419326+x72x@users.noreply.github.com>
2023-09-12 02:45:57 +0300

    Upload get_all_keys.py

--- examples/get_all_keys.py
+++ examples/get_all_keys.py
@@ -1,1 +1,10 @@
+from zjson import AsyncClient
 
+app = AsyncClient("file.json")
+
+async def main():
+    # await app.set("hi", 1)
+    print([key async for key in app.keys()])
+    print(app.path)
+
+app.run(main())
