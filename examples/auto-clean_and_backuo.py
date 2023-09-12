1739d44bb2de27a597539f63fe0509d77468038c
Zaid _ <117419326+x72x@users.noreply.github.com>
2023-09-12 03:36:10 +0300

    Delete examples/auto-clean_and_backuo.py

--- examples/auto-clean_and_backuo.py
+++
@@ -1,9 +0,0 @@
-from zjson import AsyncClient
-
-db = AsyncClient("file.json", auto_clean_and_backup=True)
-
-async def main():
-    print(await db.get("hi"))
-    await db.set("i", 2)
-
-db.run(main())
